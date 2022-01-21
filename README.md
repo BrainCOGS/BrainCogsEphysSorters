ebfdf2275a9ef924c61c72f59becc8e4a6ad97a1
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2022-01-21 14:15:01 -0500

    updates to run ks2

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 # BrainCogsEphysSorters
 Compilation of electrophysiology sorters supported in the U19 Ephys Pipeline
 
+## Instructions to clone and setup the repository
+Since this is a repository with submodules it is needed to add `--recurse-submodules` when cloning it. So log into tiger and execute:
+```
+git clone --recurse-submodules git@github.com:BrainCOGS/BrainCogsEphysSorters.git
+```
 
-## Instructions to clone the repository
-Since this is a repository with submodules it is needed to add `--recurse-submodules` when cloning it:
+## Set up python environment:
+First, make an environment that slurm can fire up:
+```
+module load anaconda3/2020.11
+conda create --name sorter-env numpy scipy matplotlib --channel conda-forge
+```
+Test by executing
+```
+conda activate sorter-env
+```
+You can choose a different version/name for your environment, but make sure to adjust them in the `slurm.job` file as well.
 
-`git clone --recurse-submodules git@github.com:BrainCOGS/BrainCogsEphysSorters.git`
+## Set up CatGT
+For preprocessing, we currently support CatGT:
+```
+wget https://billkarsh.github.io/SpikeGLX/Support/CatGTLnxApp.zip
+unzip ./CatGTLnxApp.zip
+cd ./CatGT-linux/
+chmod +x ./install.sh
+./install.sh
+```
---
+++ run_job.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+# script that is called by the slurm engine, and performs the computation.
+
+def preprocessing(method = 'CatGT'):
+    """ Function to run preprocessing on the ePhys files
+
+    Args:
+        method (str, optional): [description]. Defaults to 'CatGT'.
+    """
+    
+    match method:
+        case 'CatGT':
+            preprocessing_string = """./runit.sh '-dir=/tigress/ms81/M010/20211119/ -run=TowersTask -g=0 -t=0 -prb_fld -prb=0 -t_miss_ok -ap -apfilter=biquad,2,300,0 -gblcar -gfix=0.40,0.10,0.02 -dest=/tigress/ms81/tmp/ -out_prb_fld'"""
+
+            os.system(preprocessing_string)
+        case _:
+            print("skipping")
+
+
+def run_sorter(method = 'ks2'):
+    """ Function to do spike sorting
+
+    Args:
+        method (str, optional): [description]. Defaults to 'ks2'.
+
+    Returns:
+        [type]: [description]
+    """
+
+    match method:
+        case 'ks2':
+            start_matlab = "module load matlab/R2020a"  # Alvaro, should we load the module here, or in the slurm file? 
+            folder = "cd /tigress/ms81/"
+            matlab_command = """matlab -singleCompThread -nodisplay -nosplash -r "addpath('/tigress/ms81/spikesorters/'); run_ks2('/tigress/ms81/catgt_towers_task_g0/','/tigress/ms81/tmp/'); exit" """
+            final_touch = "touch /tigress/ms81/catgt_towers_task_g0/slurm_sorting.flag"
+            processing_string = start_matlab + folder + matlab_command + final_touch
+            os.system(processing_string)
+        case _:
+            print("skipping")
+
+def main():
+    try:
+        preprocessing('CatGT')
+        run_sorter('ks2')
+        return 0
+    except:
+        return 1
+
+if __name__ == "__main__":
+    sys.exit(main())
