033b4fa187ac322e47f37b170ca5f71ca44c849e
Alvaro Luna <alvalunasan@gmail.com>
2024-07-12 15:25:53 -0600

    running iblenv scirpt with bash-i 2

--- u19_sorting/postprocess_wrappers.py
+++ u19_sorting/postprocess_wrappers.py
@@ -27,7 +27,7 @@ class ibl_atlas_post_processing():
         ibl_output_dir = pathlib.Path(processed_data_directory, 'ibl_data')
         pathlib.Path(ibl_output_dir).mkdir(parents=True, exist_ok=True)
 
-        command = "bash -i " + config.ibl_atlas_shell_script.as_posix() + ' ' +\
+        command = "bash " + config.ibl_atlas_shell_script.as_posix() + ' ' +\
             config.ibl_atlas_script.as_posix() + ' ' + raw_data_directory.as_posix() +\
             ' ' + sorter_processed_directory.as_posix() + ' ' + ibl_output_dir.as_posix()
         
